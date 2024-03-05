0281a062149f846790a7ea815fe9e535dfbc074e
Ananad Shukla <anands2807@gmail.com>
2024-03-05 16:36:10 +0530

    Home.tsx page modified with section, h1, Link to /search, main

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+{/* cover image  */}
+      <section></section>
+
+{/* title and search button - more */}
+      <h1>
+        Latest Products
+        <Link to="/search" className='findmore'>More</Link>
+      </h1>
+
+{/* products from product array from database */}
+      <main></main>
+    </div>
   )
 }
 
