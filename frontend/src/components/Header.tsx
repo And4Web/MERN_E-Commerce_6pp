97faa0bea3c1128290b8ac2aa1d5978f131316a4
Ananad Shukla <anands2807@gmail.com>
2024-03-05 15:28:35 +0530

    Header componenet created using  sass styling, react icons. logout and user functionality will be done in future after firebase authentication implementation.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
+const user = {id: "asdf", role: "admin"}
 
 function Header() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const logoutHandler = () => {
+    setIsOpen(false)
+    console.log("Header.js logoutHandler")
+  }
   return (
-    <div>Header</div>
+    <nav className='header'>
+      <Link to="/" onClick={()=>setIsOpen(false)}>Home</Link>
+      <Link to={"/search"} onClick={()=>setIsOpen(false)}><FaSearch/></Link>
+      <Link to={"/cart"} onClick={()=>setIsOpen(false)}><FaShoppingBag/></Link>
+      {user?.id? (
+        <>
+          <button onClick={()=>setIsOpen(!isOpen)}><FaUser/></button>
+          <dialog open={isOpen}>
+            <div>
+              {user?.role === "admin" && (
+                <Link to="/admin/dashboard" onClick={()=>setIsOpen(false)}>Admin</Link>
+              )}
+              <Link to={"/orders"} onClick={()=>setIsOpen(false)}>Orders</Link>
+              <button onClick={logoutHandler}><FaSignOutAlt/></button>
+            </div>
+          </dialog>
+        </>
+      ):(
+        <Link to="/login">
+          <FaSignInAlt/>
+        </Link>
+      )}
+    </nav>
   )
 }
 
