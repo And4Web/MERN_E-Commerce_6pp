c375f96b53eacbaaa872a7b841535a3b17d90dfa
Ananad Shukla <anands2807@gmail.com>
2024-03-09 15:22:14 +0530

    multer middleware created for uploading files before creating products APIs

--- backend/dist/middlewares/auth.js
+++ backend/dist/middlewares/auth.js
@@ -4,13 +4,9 @@ import { TryCatch } from "./error.js";
 // middleware to make sure only admin is allowed
 export const adminOnly = TryCatch(async (req, res, next) => {
     const { id } = req.query;
-    if (!id) {
+    const user = await User.findById(id);
+    if (!id || !user || user.role !== "admin") {
         return next(new ErrorHandler("Unauthorized", 401));
     }
-    const user = await User.findById(id);
-    if (!user)
-        return next(new ErrorHandler("Unauthorized", 401));
-    if (user.role !== "admin")
-        return next(new ErrorHandler("Unauthorized", 401));
     next();
 });
--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -6,15 +6,11 @@ import { TryCatch } from "./error.js";
 // middleware to make sure only admin is allowed
 export const adminOnly = TryCatch(async(req, res, next)=>{
   const {id} = req.query;
+  const user = await User.findById(id);
 
-  if(!id){
+  if(!id || !user || user.role !== "admin"){
     return next(new ErrorHandler("Unauthorized", 401))
   }
-
-  const user = await User.findById(id);
-  if(!user) return next(new ErrorHandler("Unauthorized", 401))
-
-  if(user.role !== "admin") return next(new ErrorHandler("Unauthorized", 401))
-
+  
   next();
 })
