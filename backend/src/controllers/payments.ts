6734ac457a860d6858e9d65ce9cfd2ea00fdd371
Ananad Shukla <anands2807@gmail.com>
2024-03-12 15:50:11 +0530

    newCoupon, getAllCoupons for admin only, deleteCoupon for admin only and applyDiscount routes created

--- backend/src/controllers/payments.ts
+++ backend/src/controllers/payments.ts
@@ -15,3 +15,41 @@ export const newCoupon = TryCatch(async (req, res, next)=>{
 
   return res.status(201).json({success: true, message: "Coupon created successfully.", couponCode, amount});
 })
+
+// route - /api/v1/payments/discount
+export const applyDiscount = TryCatch(async (req, res, next) => {
+  const {couponCode} = req.query;
+
+  const coupon = await Coupon.findOne({couponCode});
+
+  if(!coupon) return next(new ErrorHandler("Invalid coupon", 404));
+
+  const discountAmount = coupon.amount;
+
+  return res.status(200).json({success: true, discountAmount});
+
+})
+
+export const allCoupons = TryCatch(async (req, res, next) => {
+  const allCoupons = await Coupon.find({}, {__v:0});
+  let message = "";
+
+  if(!allCoupons) message = "No active coupons at this moment.";
+  else message = `${allCoupons.length} active coupons at this moment.`
+
+  return res.status(200).json({success: true, message, allCoupons});
+})
+
+// route - api/v1/payments/coupon/:couponId
+export const deleteCoupon = TryCatch(async (req, res, next) => {
+  const id = req.params.couponId;
+
+  if(!id) return next(new ErrorHandler("Invalid coupon Id", 404));
+
+  const coupon = await Coupon.findById(id);
+  if(!coupon) return next(new ErrorHandler("No coupon with such Id found.", 404));
+
+  await coupon.deleteOne();
+
+  return res.status(200).json({success: true, message: "Coupon deleted successfully.", coupon})
+})
--- backend/src/routes/paymentsRoutes.ts
+++ backend/src/routes/paymentsRoutes.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
-import { newCoupon } from "../controllers/payments.js";
+import { allCoupons, applyDiscount, deleteCoupon, newCoupon } from "../controllers/payments.js";
 
 const router = express.Router();
 
 // route - /api/v1/payments/coupon/new
 router.post("/coupon/new", newCoupon)
 
+// route - /api/v1/payments/all-coupons
+router.get("/all-coupons",adminOnly, allCoupons)
+
+// route - api/v1/payments/coupon/:couponId
+router.delete("/coupon/:couponId",adminOnly, deleteCoupon) 
+
+// route - /api/v1/payments/discount
+router.get("/discount", applyDiscount);
 
 
 export default router;
